3232169313ecee615efd1a86064fa50e2d2047a2
Himanshu Awasthi <83570704+HimanshuAwasthi24@users.noreply.github.com>
2024-11-30 14:01:21 +0530

    Delete adhoc_commands.sh

--- adhoc_commands.sh
+++
@@ -1,9 +0,0 @@
-#ping module
-ansible -i inventory.ini -m ping all
-ansible -i inventory.ini -m ping db
-ansible -i inventory.ini -m ping ubuntu@192.168.200.16
-
-#shell module
-ansible -i inventory.ini -m shell -a "sudo ls /etc" all
-
-ansible-galaxy role install roleNameFromGalaxyMarketplace
